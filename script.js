2c1a2f5289049a96e53fc518e498595632520157
NahomTeshome <nahomte4@gmail.com>
2025-11-06 15:50:42 +0100

    Adding sound and collision detection

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 mouseX = null;
 mouseY = null;
 
+
+let gameStarted = false;
 var canvas;
 var ctx;
 var canvaswidth;
 
 var playerX = 20;
 var playerY;
-var computerX = 270;
-var computerY = 40;
+
+var computerX =  660;
+var computerY = 170;
+
+var ballX = 700/2 - 10;
+var ballY = 400/2 - 10;
+
+var ballVelocityX;
+
+let hitSound = new Audio("wallhit.wav");
+
 function loaded(params) {
      canvas = document.getElementById("canvas");
      ctx = canvas.getContext("2d");
      canvaswidth = canvas.offsetWidth;
      ctx.fillStyle = "#a7b2c2";
-     ctx.fillRect(20,20,10,50);
- 
-      
+    
+     ballVelocityX = setRandomSpeed();
+}
+
+
+function setRandomSpeed(params) {
+    let randInt = Math.floor(Math.random() * 2);
+    if (randInt == 0){
+        return -10;
+     }
+    if (randInt == 1) {
+        return  10;
+     }
 }
 onmousemove = function(e){
     console.log("Mouse Location" , e.clientX, e.clientY);
@@ -26,21 +47,59 @@ onmousemove = function(e){
 
 }
 
-setInterval(onTimerTick, 33);
+document.addEventListener("click", () => {
+    if (!gameStarted) {
+        gameStarted = true;
+        intervalId = setInterval(onTimerTick, 33);
+        hitSound.play(); // optional: unlock audio
+        console.log("Game started!");
+    }
+});
 function onTimerTick() {
 
-    ctx.clearRect(0,0,600,400);
+    ctx.clearRect(0,0,700,400);
     ctx.fillStyle = "#a7b2c2";
     playerY = mouseY - 200;
-    ctx.fillRect(playerX,playerY,10,50);
-    ctx.fillRect(computerX,computerY,10,50);
 
+    if (playerY <= 0) {
+        playerY = 0;
+        console.log("below zero");
+    }
+    if (playerY >= 300) {
+        playerY = 300;
+    }
+
+    if (ballX <= 0){
+        ballX = 340;
+        hitSound.play();
+        ballVelocityX = setRandomSpeed();
+    }
+
+    if (ballX >= 700){
+        ballX = 340;
+        hitSound.play();
+        ballVelocityX = setRandomSpeed();
+    }
+    if (ballX <= 30) {
+        if (ballY >= playerY && ballY <= playerY +100)
+        {
+            ballVelocityX = ballVelocityX * -1;
+        }
+    }
+
+    ctx.fillRect(playerX,playerY,10,100);
+    ctx.fillRect(computerX,computerY,10,100);
+
+    ctx.fillRect(ballX,ballY,20,20);
+    
     if (playerY > computerY){
-        computerY += 1;
+        computerY += 4;
     }
     if (playerY < computerY) {
-        computerY -= 1;
+        computerY -= 4;
     }
+    ballX += ballVelocityX;
+    console.log("player y is",playerY);
 }
         
 
